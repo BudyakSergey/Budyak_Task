6b88824e92f1a725750c7774b464c4ba1d8b4c27
Budyak Sergey <89227322222@mail.ru>
2022-12-21 17:06:11 +0500

    Added Task03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -5,16 +5,11 @@
 // a = 2, b = 10 -> нет
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
-// 3. Напишите программу, которая будет выдавать
-// название дня недели по заданному номеру.
-// 3 -> Среда
-// 5 -> Пятница
 
 Console.Write("Введите первое целое число: ");
 int NumbersA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе целое число: ");
 int NumbersB = Convert.ToInt32(Console.ReadLine());
-
 if (NumbersA == NumbersB * NumbersB)
 {
     Console.WriteLine($"Число {NumbersA} является квадратом числа {NumbersB}");
