5097e19584f18f6535a734eb94b88f4b24fe65c6
Budyak Sergey <89227322222@mail.ru>
2022-12-21 17:55:11 +0500

    Finish Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = -a;
+// Console.Write($"{numbersB}, ");
+while (count != a+1)
+// Console.Write(numbersB);
+{
+    Console.Write(count + " ");
+    count = count + 1;
+}
