86a60340227b7debd9ffc56ac196662863e55c8c
Budyak Sergey <89227322222@mail.ru>
2022-12-23 00:19:14 +0500

    Finish Task 10, 13, 15

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число от 100-999 => {number}");
+
+// int numberB = number/10 % 10;
+// Console.WriteLine($"Десток от числа {number} = {numberB}");
+
+int secondDigit = SecondDigit(number);
+Console.WriteLine($"Вторая цифра в этом чиcле = {secondDigit}");
+
+int SecondDigit(int num) 
+{
+    int result = num/10 % 10;
+    return result;
+}
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет. Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// int number = new Random().Next(0, 100000000);
+// Console.WriteLine($"Случайное число от 0-99999999 => {number}");
+// ввод числа вручную
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// построчный код
+// при выводе исходных данных для точного понимания из какого условия
+// пишутся ответ, условия пронумерованы 1, 2, 3
+// int result = 0;
+// if (number < 100)
+// {
+//     Console.WriteLine("В выбранном числе третей цифры нет");
+// }
+// else if (number >= 100 && number <= 999)
+// {
+//     Console.WriteLine($"1Третья цифра в этом чиcле = {number % 100 % 10}");
+// }
+// else if (number >= 1000 && number <= 9999)
+// {
+//     Console.WriteLine($"2Третья цифра в этом чиcле = {number / 10 % 10}");
+// }
+// else
+// {
+//     int discharge = 10;
+//     while (number > 9999)
+//     {
+//         result = number / 100 % 10;
+//         // Console.WriteLine(result + " первый результат"); // для проверки
+//         number = number / discharge;
+//         // Console.WriteLine(number + " "); // для проверки
+//         // Console.WriteLine(result + " итоговый "); // для проверки
+//     }
+//     Console.WriteLine($"3Третья цифра в этом чиcле  = {result}");
+// }
+
+// через функцию
+
+int res = ThirdGrade(number);
+Console.WriteLine(res == -1 ? "В выбранном числе третей цифры нет" : $"Третья цифра в этом чиcле  = {res}");
+
+int ThirdGrade(int num)
+{
+    int result = 0;
+    if (num < 100)
+    {
+        result = -1;
+    }
+    else if (num >= 100 && num <= 999)
+    {
+        result = num % 100 % 10;
+    }
+    else if (num >= 1000 && num <= 9999)
+    {
+        result = num / 10 % 10;
+    }
+    else
+    {
+        int discharge = 10;
+        while (num > 9999)
+        {
+            result = num / 100 % 10;
+            num = num / discharge;
+        }
+    }
+    return result;
+}
