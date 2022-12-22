1018b882dbad1861b3684d31a8cdb5ae673920a0
Budyak Sergey <89227322222@mail.ru>
2022-12-22 21:05:36 +0500

    Finish Task 14 и 16

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число кратность которого будем проверять: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число на кратность которого будем проверять: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число на кратность которого будем проверять: ");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(BothMultiple(numberFirst, numberSecond, numberThird) ? "Да, кратно" : "Нет, не кратно");
+
+bool BothMultiple(int num1, int num2, int num3)
+{
+    return num1 % num2 == 0 && num1 % num3 == 0;
+}
