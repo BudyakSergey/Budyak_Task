54d21802937c6bd6697fbe54094f09fe4dac2667
Budyak Sergey <89227322222@mail.ru>
2022-12-22 13:56:32 +0500

    Finish Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = numberA % 2;
+// Console.Write($"{numberB}");
+if (numberB == 0)
+{
+    Console.Write($"Число {numberA} четное");
+}
+else
+{
+     Console.Write($"Число {numberA} не четное");
+}
