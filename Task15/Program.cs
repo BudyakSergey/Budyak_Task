33f10f46e440662b41e9d8e31ed904fa57ba0aeb
Budyak Sergey <89227322222@mail.ru>
2022-12-23 00:19:35 +0500

    Finish Task 10, 13, 15

---
+++ Task15/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите целое положительное число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = DayNumber(number);
+if (res == -1)
+{
+    Console.WriteLine("Вы ввели число не удовлетворящее условиям");
+}
+else
+{
+    Console.WriteLine(res == 0 ? "Этот день - выходной" : "Этот день - рабочий");
+}
+
+int DayNumber(int num)
+{
+    int day = 1;
+    if (num < 1 || num > 7)
+    {
+        day = -1;
+    }
+    else if (num == 6 || num == 7)
+    {
+        day = 0;
+    }
+    return day;
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,19 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите первое число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Square(numberFirst, numberSecond) ? "Да" : "Нет");
+
+bool Square(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
