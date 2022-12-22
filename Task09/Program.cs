edb9d442c65fc6b5f37cfd7d9a0dcebc9bca2c40
Budyak Sergey <89227322222@mail.ru>
2022-12-22 18:04:52 +0500

    Finish Task 09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,11 +5,21 @@
 // 12-> 2
 // 85 -> 8
 
-int number = new Random().Next(10,100);
+int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
-int firstDigit = number/10; 
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 // if (firstDigit > secondDigit) Console.WriteLine ($"Наибольшая цифра цисла {number} => {firstDigit}");
 // else Console.WriteLine ($"Наибольшая цифра цисла {number} => {secondDigit}");
-int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine ($"Наибольшая цифра цисла {number} => {result}");
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра цисла {number} => {result}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра цисла {number} => {maxDigit}");
+int MaxDigit(int number) // описание метода
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
