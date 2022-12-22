8111fbf76a8e4e40b7fe9e8983cdb9e6d5d6f390
Budyak Sergey <89227322222@mail.ru>
2022-12-22 20:33:42 +0500

    Finish Task 11/12

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число от 100-999 => {number}");
+// int numberA = number/100;
+// Console.WriteLine($"Сотня от числа {number} = {numberA}");
+// int numberB = number % 100;
+// int numberC = numberB % 10;
+
+// Console.WriteLine($"Сотня от числа {number} = {numberC}");
+// int result = numberA*10+numberC;
+// Console.WriteLine(result);
+
+
+int maxDigit = Itog(number);
+ Console.WriteLine(maxDigit);
+
+int Itog(int number) // описание метода
+{
+    int numberA = number / 100;
+    int numberC = number % 10;
+    int result = numberA*10+numberC;
+    return result;
+}
--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+// через код
+// if (numberFirst % numberSecond == 0)
+// {
+//     Console.WriteLine($"{numberFirst} кратно {numberSecond}");
+// }
+// else
+// {
+//     Console.WriteLine($"{numberFirst} не кратно {numberSecond}");
+//     Console.WriteLine($"отстаток от деления {numberFirst % numberSecond}");
+// }
+
+// через функцию
+int result = Multiplicity(numberFirst, numberSecond);
+Console.WriteLine(result == 0 ?  $"{numberFirst} кратно {numberSecond}" : $"{numberFirst} не кратно {numberSecond}, отстаток от деления {result}");
+
+int Multiplicity(int numberFirst, int numberSecond)
+{
+    return numberFirst % numberSecond;
+}
