96acc863d85f0a474a87bc84bcdc5f924d6a575d
Budyak Sergey <89227322222u@mail.ru>
2023-01-29 01:10:28 +0500

    Finish Task 40-45

---
+++ Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Определите количество вводимых чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+if (size < 0)
+{
+    Console.WriteLine($"Количество вводимых чисел должно быть больше или равно 0");
+
+}
+else
+{
+    int[] array = CreateArrayRndIntAndPositiveNumber(size);
+    Console.WriteLine($"В данном спике {array[0]} положительных чисел");
+}
+
+int[] CreateArrayRndIntAndPositiveNumber(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    // return arr;
+    int[] result = new int[1];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) result[0] = result[0] + 1;
+    }
+    return result;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Задайте коэффициенты первой прямой, которая описана уровнением y = k1 * x + b1: ");
+Console.Write("Задайте коэффициент k1: ");
+int coefficientK1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте коэффициент b1: ");
+int coefficientB1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте коэффициенты второй прямой, которая описана уровнением y = k2 * x + b2: ");
+Console.Write("Задайте коэффициент k2: ");
+int coefficientK2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте коэффициент b2: ");
+int coefficientB2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (coefficientK2 - coefficientK1 == 0)
+{
+    Console.WriteLine("Эти прямые не пересекаются");
+    return;
+}
+
+
+double[] intersectionPoint = IntersectionPoint(coefficientK1, coefficientB1, coefficientK2, coefficientB2);
+Console.Write("Точка пересечения прямых - ");
+PrintArray(intersectionPoint);
+
+
+double[] IntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    double[] result = new double[2];
+    result[0] = Math.Round((b1 - b2) / (k2 - k1), 2, MidpointRounding.ToZero);
+    result[1] = Math.Round((k1 * result[0] + b1), 2, MidpointRounding.ToZero);
+    return result;
+}
+
+
+void PrintArray(double[] arr)
+{
+    Console.Write("(");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine(")");
+}
